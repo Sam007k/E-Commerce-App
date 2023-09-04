9bb14cdcac90a0b3508fbc438aa390b747028de6
sam007k <58684781+Sam007k@users.noreply.github.com>
2023-09-04 13:14:36 +0000

    front-end-complete

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <>
+      <Home />
+      {/* <ProductList/> */}
+      {/* <Product/> */}
+      {/* <Register/> */}
+      {/* <Login/> */}
+      {/* <Cart /> */}
+    </>
+  );
+}
+
+export default App;
