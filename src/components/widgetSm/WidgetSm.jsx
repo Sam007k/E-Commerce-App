1b1c4a0f3eb1a0b1eea641b3370bbcc9b406fab3
Sam007k <msameer007k@gmail.com>
2023-10-02 23:59:38 +0530

    adm

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { userRequest } from "../../requestMethods";
+
+export default function WidgetSm() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get("users/?new=true");
+        setUsers(res.data)
+      } catch (err) {}
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {users.map((user) => (
+          <li className="widgetSmListItem">
+            <img
+              src={
+                user.img ||
+                "https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg "
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+              <span className="widgetSmUserTitle">{user.title}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
