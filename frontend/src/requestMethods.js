1beb035692b65996ec8785b743b7439d68da257f
Sameer <58684781+Sam007k@users.noreply.github.com>
2023-10-29 16:33:34 +0530

    Update requestMethods.js

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = `http://localhost:5001/api/`;
+// const BASE_URL = `http://localhost:5001/api/`;
+const BASE_URL = `https://e-commerce-app-api-gray.vercel.app/api/`;
+
 // const BASE_URL = `http://wtl4yt-5001.csb.app/api/`;
 
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
