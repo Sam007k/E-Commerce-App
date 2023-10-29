652196bdc662819c70904fcaac6149314c40dfe2
Sam007k <msameer007k@gmail.com>
2023-10-29 16:20:12 +0530

    vercel.json_requestmethods

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-// const BASE_URL = `http://localhost:5001/api/`;
-const BASE_URL = `https://wtl4yt-5001.csb.app/api/`;
+const BASE_URL = `http://localhost:5001/api/`;
+// const BASE_URL = `https://wtl4yt-5001.csb.app/api/`;
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken || ''
-  
-  
+const TOKEN =
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+    .accessToken ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZmM1ZjRkNzk4ODc1OTExMDE1ODkyNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5ODUwNjAxNCwiZXhwIjoxNjk4NzY1MjE0fQ.LQhZlKzo98Omj_AxNcHppiSvZGp1gzEhVXmAX7m38nA";
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
